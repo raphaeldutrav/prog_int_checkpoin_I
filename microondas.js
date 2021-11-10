7f4e2e657c066d5ad1a6394c97d162a4cd85a0a9
Raphael Dutra <89048522+raphaeldutrav@users.noreply.github.com>
2021-11-09 21:43:55 -0300

    Exercicio_checkpoint_1
    function pipoca (valor) {
        if(valor >= 30){
            return 'Kabumm!\n';
        }else if(valor = 20){
            return 'A comida queimou!\n';
        }else if(valor = 10){
            return 'Prato pronto, bom apetite!\n';
        }else if(valor <= 10){
            return 'tempo insuficiente!\n'
        }
    }
    function macarrao (valor){
        if(valor >= 24){
            return 'Kabumm!\n';
        }else if(valor = 16){
            return 'A comida queimou!\n';
        }else if(valor = 8){
            return 'Prato pronto, bom apetite!\n';
        }else if(valor <= 8){
            return 'tempo insuficiente!\n'
        }
    }
    function carne (valor){
        if(valor >= 45){
            return 'Kabumm!\n';
        }else if(valor = 30){
            return 'A comida queimou!\n';
        }else if(valor = 15){
            return 'Prato pronto, bom apetite!\n';
        }else if(valor <= 15){
            return 'tempo insuficiente!\n'
        }
    }
    function feijao (valor){
        if(valor >= 36){
            return 'Kabumm!\n';
        }else if(valor = 24){
            return 'A comida queimou!\n';
        }else if(valor = 12){
            return 'Prato pronto, bom apetite!\n';
        }else if(valor <= 12){
            return 'tempo insuficiente!\n'
        }
    }
    function brigadeiro (valor){
        if(valor >= 24){
            return 'Kabumm!\n';
        }else if(valor = 16){
            return 'A comida queimou!\n';
        }else if(valor = 8){
            return 'Prato pronto, bom apetite!\n';
        }else if(valor <= 8){
            return 'tempo insuficiente!\n'
        }
    }
    
    
    console.log(pipoca(30));

---
+++ microondas.js
@@ -0,0 +1,58 @@
+function pipoca (valor) {
+    if(valor >= 30){
+        return 'Kabumm!\n';
+    }else if(valor = 20){
+        return 'A comida queimou!\n';
+    }else if(valor = 10){
+        return 'Prato pronto, bom apetite!\n';
+    }else if(valor <= 10){
+        return 'tempo insuficiente!\n'
+    }
+}
+function macarrao (valor){
+    if(valor >= 24){
+        return 'Kabumm!\n';
+    }else if(valor = 16){
+        return 'A comida queimou!\n';
+    }else if(valor = 8){
+        return 'Prato pronto, bom apetite!\n';
+    }else if(valor <= 8){
+        return 'tempo insuficiente!\n'
+    }
+}
+function carne (valor){
+    if(valor >= 45){
+        return 'Kabumm!\n';
+    }else if(valor = 30){
+        return 'A comida queimou!\n';
+    }else if(valor = 15){
+        return 'Prato pronto, bom apetite!\n';
+    }else if(valor <= 15){
+        return 'tempo insuficiente!\n'
+    }
+}
+function feijao (valor){
+    if(valor >= 36){
+        return 'Kabumm!\n';
+    }else if(valor = 24){
+        return 'A comida queimou!\n';
+    }else if(valor = 12){
+        return 'Prato pronto, bom apetite!\n';
+    }else if(valor <= 12){
+        return 'tempo insuficiente!\n'
+    }
+}
+function brigadeiro (valor){
+    if(valor >= 24){
+        return 'Kabumm!\n';
+    }else if(valor = 16){
+        return 'A comida queimou!\n';
+    }else if(valor = 8){
+        return 'Prato pronto, bom apetite!\n';
+    }else if(valor <= 8){
+        return 'tempo insuficiente!\n'
+    }
+}
+
+
+console.log(pipoca(30));
